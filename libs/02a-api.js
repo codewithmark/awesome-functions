8f749d9088e9586f203c5bd02835c57581293085
Code With Mark <mark2kumar@gmail.com>
2021-06-15 14:30:24 -0400

    Add files via upload

---
+++ libs/02a-api.js
@@ -0,0 +1,48 @@
+
+//--->API Call Functions - Start
+
+var api = function(){}; 
+
+	
+
+  //--->User Location Info Function - Start
+  api.UserLoc = function(callback) 
+  {
+    var deferred = new jQuery.Deferred(); 
+    if (jQuery.isFunction(callback)) 
+    {
+      deferred.then(callback);
+    }
+    //Make the api call
+
+    jQuery.getJSON("https://apimk.com/ip.json", function(data, status)
+    {   		 
+    	deferred.resolve(data);
+    });
+    	
+      return deferred.promise();
+  };
+
+  //--->User Location Info Function - End
+  
+  //--->User Device Access Function - Start
+  api.IsMobile = function(callback)   
+  {
+    var deferred = new jQuery.Deferred(); 
+    if (jQuery.isFunction(callback)) 
+    {
+      deferred.then(callback);
+    }
+	//Make the api call
+	jQuery.getJSON("https://apimk.com/ismobile.json", function(data, status)
+	{  		
+		deferred.resolve(data);
+
+	}); 
+    return deferred.promise();
+  }; 
+
+  //--->User Device Access Function - End  	
+	 
+    
+//--->API Call Functions - End 
